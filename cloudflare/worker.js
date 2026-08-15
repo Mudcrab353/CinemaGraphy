f4501b25cfacbe4385e83b469f2865163aa81f6f
NerdCow <84755104+TheNerdCow@users.noreply.github.com>
2026-08-15 05:49:42 +0330

    آپدیت 2.0.3

--- cloudflare/worker.js
+++ cloudflare/worker.js
@@ -13,7 +13,7 @@ import {createFetchHttpClient} from './http-client.js'
 import {createWorkerProxyConfig, handleProxyRequest} from './proxy.js'
 
 const ADDON_PREFIX = 'ip'
-const ADDON_VERSION = '2.0.2'
+const ADDON_VERSION = '2.0.3'
 
 const CATALOGS = [
     {key: 'f2media', name: 'F2Media', catalogType: 'movies'},
